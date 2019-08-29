c1a5b6b8dbe17364d1c79a59c3e5eae68837b122
Jackson  Ricks <jacksonricks@cjca-23915d.gonzaga.edu>
2019-08-29 10:11:18 -0700

    new update

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,6 +1,6 @@
 public class HelloWorld2
 {
     public static void main (String[] args) {
-        System.out.println("My second hello world");
+        System.out.println("A third and final change to Hello World");
     }
 }
